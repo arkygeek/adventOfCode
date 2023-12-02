3dcaa3a5294d8e5b30003c633fad51ed2526bffa
Dr. Jason S. Jorgenson <jjorgenson@gmail.com>
2023-12-02 07:36:42 -0700

    tidied up a bit

--- day2a.py
+++ day2a.py
@@ -16,31 +16,32 @@ import re
 from collections import defaultdict
 
 # Initialize the sum of the powers of the minimum sets of cubes
-sumOfPowers: int = 0
+mySumOfPowers: int = 0
 
 # Open and read the input file
 with open('input2.txt', 'r') as file:
     for line in file:
-        print(line)  # Add this line to print the lines being read
+        # print(line)  # Add this line to print the lines being read
         match = re.match(r'Game (\d+): (.*)', line)
-        print(match)  # Add this line to print the match object
+        # print(match)  # Add this line to print the match object
+
         if match is None:
             continue
-        gameId, cubeSets = match.groups()
-        # rest of the code
-        cubeSets = cubeSets.split('; ')
+
+        myGameId, myCubeSets = match.groups()
+        myCubeSets = myCubeSets.split('; ')
 
         # Find the minimum number of cubes of each colour that must have been in the bag
-        minCubes = defaultdict(int)
-        for cubeSet in cubeSets:
-            cubes = re.findall(r'(\d+) (\w+)', cubeSet)
-            for count, colour in cubes:
-                minCubes[colour] = max(minCubes[colour], int(count))
+        myMinCubes = defaultdict(int)
+        for theCubeSet in myCubeSets:
+            myCubes = re.findall(r'(\d+) (\w+)', theCubeSet)
+            for myCount, myColour in myCubes:
+                myMinCubes[myColour] = max(myMinCubes[myColour], int(myCount))
 
         # Calculate the power of the minimum set of cubes and add it to the sum
-        power = minCubes['red'] * minCubes['green'] * minCubes['blue']
-        print(minCubes)  # Add this line to print the minCubes dictionary
-        print(power)  # Add this line to print the power variable
-        sumOfPowers += power
+        myPower = myMinCubes['red'] * myMinCubes['green'] * myMinCubes['blue']
+        # print(myMinCubes)  # print the minCubes dictionary
+        # print(myPower)  # print the power variable
+        mySumOfPowers += myPower
 
-print(f"Sum of the powers of the minimum sets of cubes: {sumOfPowers}")
+print(f"Sum of the powers of the minimum sets of cubes: {mySumOfPowers}")
