a6a8938653ab29d09051cd34bc024af287e590fc
Jason Jorgenson <jjorgenson@gmail.com>
2023-12-18 11:26:34 -0700

    day 8 part 1 solved

--- day8/day8.py
+++ day8/day8.py
@@ -1,61 +1,62 @@
-# Day 8
+import itertools
+
+ourInputData = open('day8/input8.txt').read().splitlines()
 
 import itertools
 
-# ourInputData = open('day8/input8.txt').read().splitlines()
-ourInputData = open('day8/input8.txt').read().splitlines()
-
-
 class PuzzleOne:
-    def __init__(self):
-        # define the object properties
-        self.InputData = ourInputData
-        print(f'Input Data: {self.InputData}')
+    def __init__(self, theInputData):
+        self.sInputData = theInputData
+        self.process_data()
 
+    # Get the data into a form we can use
     def process_data(self):
         # Split the instructions from the data
-        self.instructions = self.InputData[0]
-        data = [line.split(' = ') for line in self.InputData[1:] if line]
-        self.data = [(item, tuple(values.strip('()').split(', '))) for item, values in data]
-
-
-    def find_steps_to_ZZZ(self, instructions, data):
-        # Create a dictionary from the data
-        myDataDict = {item: (left, right) for item, (left, right) in data}
-
-        # Initialize the current item and the number of steps
-        myCurrentItem = list(myDataDict.keys())[0]
-        mySteps = 0
-
-        # Create an iterator that produces the instructions indefinitely
-        instructions_cycle = itertools.cycle(instructions)
-
-        # Iterate over the instructions
-        while myCurrentItem != 'ZZZ':
-            # Get the next instruction
-            instruction = next(instructions_cycle)
-
-            # Follow the instruction
-            if instruction == 'L':
-                myCurrentItem = myDataDict[myCurrentItem][0]
-            else:  # instruction == 'R'
-                myCurrentItem = myDataDict[myCurrentItem][1]
-
-            # Increment the number of steps
-            mySteps += 1
-
-        # Return the number of steps
-        return mySteps
-
+        self.sInstructions = self.sInputData[0]
+        myData = [eaLine.split(' = ') for eaLine in self.sInputData[1:] if eaLine]
+            # self.InputData[1:]
+            # This slices the list starting from the second element (index 1),
+            # skipping the first element (index 0)
+        self.sData = {eaItem: tuple(eaValue.strip('()').split(', ')) for eaItem, eaValue in myData}
+        return self.sData
 
     def calculate_answer(self):
-        # calculate the answer
-        pass
+        # Initialize the current item, the number of steps, and a stack for DFS
+        myStartingItem = 'AAA'
+        mySteps = 0
+        myStack = [(myStartingItem, mySteps)]
 
+        # Create an iterator that produces the instructions indefinitely
+        myInstructionCycle = itertools.cycle(self.sInstructions)
 
+        while myStack:
+            myCurrentItem, mySteps = myStack.pop()
 
-# Puzzle One
-myPuzzle1 = PuzzleOne()
-myPuzzle1.process_data()
-print(myPuzzle1.find_steps_to_ZZZ(myPuzzle1.instructions, myPuzzle1.data))
+            # If we've reached 'ZZZ', return the number of steps
+            if myCurrentItem == 'ZZZ':
+                return mySteps
 
+            # Get the next instruction
+            myNextInstruction = next(myInstructionCycle)
+
+            # Follow the instruction and push the next item to the stack
+            if myNextInstruction == 'L':
+                myNextItem = self.sData[myCurrentItem][0]
+            else:  # instruction == 'R'
+                myNextItem = self.sData[myCurrentItem][1]
+
+            myStack.append((myNextItem, mySteps + 1))
+
+        # If 'ZZZ' is not reachable, return -1
+        print('ZZZ is not reachable')
+        return -1
+
+# build in test cases
+# myPuzzle1_dfs1 = PuzzleOne(['LLR', '', 'AAA = (BBB, BBB)', 'BBB = (AAA, ZZZ)', 'ZZZ = (ZZZ, ZZZ)'])
+# print(myPuzzle1_dfs1.calculate_answer())  # Expected Output: 6
+
+myPuzzle1_dfs = PuzzleOne(ourInputData)
+myPuzzle1_dfs.process_data()
+
+# Call calculate_answer
+print(myPuzzle1_dfs.calculate_answer())
